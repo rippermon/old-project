afa341c307849c224c879f646b33d56ebfb2ca41
Ripper Sir <92292759+Ziyankp@users.noreply.github.com>
2021-12-09 23:04:55 +0530

    Update rashmika.js

--- plugins/rashmika.js
+++ plugins/rashmika.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 ameer-kallumthodi.
+/* Copyright (C) 2021 Ziyankp.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Pikachu-Ameer Suhail
+RIPPER-SER
 */
 
 const Asena = require('../events');
