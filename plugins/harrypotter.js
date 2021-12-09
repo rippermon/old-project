1397da66d9e2b4fc98ffdff7a4563e49d65a2418
Ripper Sir <92292759+Ziyankp@users.noreply.github.com>
2021-12-09 22:21:36 +0530

    Update harrypotter.js

--- plugins/harrypotter.js
+++ plugins/harrypotter.js
@@ -8,7 +8,7 @@ const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys'
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-const need = "Command Kazhinn Yanthankilum Eyuth muthe\n🙄🙄🙄"
+const need = "Command Kazhinn enthenkilum type muthe\n🤭🤭"
 
 if (Config.WORKTYPE == 'private') {
 
