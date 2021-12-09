8b33b06baaf2784e80c800646bdd10b9182cc5bf
Ripper Sir <92292759+Ziyankp@users.noreply.github.com>
2021-12-09 22:13:05 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -9,12 +9,12 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*RIPPERSER bot created by Ziyan*
+*Creator number : wa.me/919446072492?text=Hi%20Ziyan%20bro.%20*
 
 *Bot setting video : https://youtu.be/_D4ZYuUSXjs*
 
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
+*Githublink (Setup)  :    https://github.com/Ziyankp/RIPPER-SER*
 
 *Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
 
